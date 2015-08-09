097962942c01b11c20fed7e0f8d077fa13fc373d
Andrew Radev <andrey.radev@gmail.com>
2015-08-09 17:36:49 +0300

    Multiple comments with a simple "model"

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -3,14 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     postComment: function() {
-      var comment = {
-        email: this.get('email'),
-        body:  this.get('body')
-      };
-
       var controller = this;
-      $.post('http://localhost:4567/comments', comment, function() {
+      $.post('http://localhost:4567/comments', this.get('model'), function() {
+        controller.set('model', {});
         controller.set('commentMessage', "You've posted a comment!");
+
+        setTimeout(function() { controller.set('commentMessage', null); }, 2000);
       });
     }
   }
