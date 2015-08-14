394aec763c283b68a6390f03afc7310772a8e388
Andrew Radev <andrey.radev@gmail.com>
2015-08-14 11:55:48 +0300

    Better contact form

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -3,13 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     postComment: function() {
-      var controller = this;
+      var self = this;
       Ember.$.post('http://localhost:4567/comments', this.get('model'), function() {
-        controller.set('model', {});
-        controller.set('commentMessage', "You've posted a comment!");
-
-        setTimeout(function() { controller.set('commentMessage', null); }, 2000);
+        self.set('model', {});
+        self.set('commentMessage', "You've posted a comment!");
       });
-    }
+    },
+
+    removeMessage: function() {
+      this.set('commentMessage', null);
+    },
   }
 });
