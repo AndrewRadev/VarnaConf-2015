cf4d9130e5ade1069009241196c66ff3346b21c3
Andrew Radev <andrey.radev@gmail.com>
2015-08-14 12:16:37 +0300

    Initial, almost-working, registration page

---
+++ app/controllers/users/new.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    createUser: function() {
+      this.get('model').save();
+    },
+  },
+});
---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  email:                DS.attr('string'),
+  password:             DS.attr('string'),
+  passwordConfirmation: DS.attr('string'),
+  firstName:            DS.attr('string'),
+  lastName:             DS.attr('string'),
+  bio:                  DS.attr('string'),
+  birthday:             DS.attr('date'),
+
+  fullName: function() {
+    var firstName = this.get('firstName') || '';
+    var lastName = this.get('lastName') || '';
+
+    return firstName + ' ' + lastName;
+  }.property('firstName', 'lastName'),
+});
