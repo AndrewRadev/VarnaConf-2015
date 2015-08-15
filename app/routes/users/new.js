d5e6328205797e52643ed606738b58641127564d
Andrew Radev <andrey.radev@gmail.com>
2015-08-15 10:38:35 +0300

    Registration wizard

--- app/routes/users/new.js
+++ app/routes/users/new.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('user');
+  },
+
+  afterModel: function() {
+    return this.transitionTo('users.new.login-info');
   }
 });
